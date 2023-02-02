9c2ea49e852ac2f5b2cb75857e9d70db3f4eff42
Yoo Seungmin <dnjfht@naver.com>
2023-02-02 13:31:12 +0900

    twitter clone project

---
+++ .history/src/firebase_20230202001756.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrMj52PF9fca_brj4fXQK9-0uBNFLYFXA",
+  authDomain: "nwitter-85f4d.firebaseapp.com",
+  projectId: "nwitter-85f4d",
+  storageBucket: "nwitter-85f4d.appspot.com",
+  messagingSenderId: "560313169532",
+  appId: "1:560313169532:web:4b621ba475ad6784b6bbf9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ .history/src/firebase_20230202001940.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrMj52PF9fca_brj4fXQK9-0uBNFLYFXA",
+  authDomain: "nwitter-85f4d.firebaseapp.com",
+  projectId: "nwitter-85f4d",
+  storageBucket: "nwitter-85f4d.appspot.com",
+  messagingSenderId: "560313169532",
+  appId: "1:560313169532:web:4b621ba475ad6784b6bbf9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
